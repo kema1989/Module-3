d28fbd6941aa44e6063e16ab21a715f8ec2d4f09
Кемран Абдрашид <kemran94875@gmail.com>
2021-07-02 17:18:44 +0600

    Добавьте файлы проекта.

---
+++ Module3App1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Module3App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myname = "Nobody";
+            Console.WriteLine(myname);
+            Console.ReadKey();
+        }
+    }
+}
