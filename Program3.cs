0836027ffcc8e6d780083208738566c10946b81c
Кемран Абдрашид <kemran94875@gmail.com>
2021-07-04 13:16:54 +0600

    gjhvh

---
+++ Program3.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Module3App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            enum Semaphore
+            {
+            RED = 100,
+            Yellow = 200,
+            Green = 300
+            }
+        }
+    }
+}
